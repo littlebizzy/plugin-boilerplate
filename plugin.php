a3e49ecc6e92eba13df916a26098273f7223c4d5
Pau <pauiglesias@gmail.com>
2018-12-14 14:59:32 +0100

    Added missing plugin meta headers from 1.0.0 version.

--- plugin.php
+++ plugin.php
@@ -8,8 +8,10 @@ Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
+PBP Version: 1.1.0
 WC requires at least: 3.3
-WC tested up to: 3.4
+WC tested up to: 3.5
+Prefix: ABCDEF
 */
 
 // Plugin namespace
