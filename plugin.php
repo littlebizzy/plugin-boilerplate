1689077719baf477029bfe4dd20be61514bc6db8
Jesse Nickles <jesse@littlebizzy.com>
2020-01-10 15:05:09 +0700

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,14 +3,14 @@
 Plugin Name: The Plugin Name
 Plugin URI: https://www.littlebizzy.com/plugins/[plugin-slug]
 Description: [Plugin description]
-Version: 1.0.0
+Version: 0.0.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
-PBP Version: 1.2.0
+PBP Version: 1.2.1
 WC requires at least: 3.3
-WC tested up to: 3.5
+WC tested up to: 3.8
 Prefix: ABCDEF
 */
 
@@ -20,7 +20,7 @@ namespace LittleBizzy\PluginNamespace;
 // Plugin constants
 const FILE = __FILE__;
 const PREFIX = '[6 chars prefix]';
-const VERSION = '1.0.0';
+const VERSION = '0.0.0';
 const REPO = '[github-account/repo-slug]';
 
 // Boot
